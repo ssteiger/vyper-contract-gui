4d97be36bf73490005752ec73c62079de19eb202
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2019-06-08 08:58:04 +0300

    fixed actions

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -1,26 +1,27 @@
 // @flow
-import { createStore, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga'
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 // lets you manage the history stack, navigate, confirm navigation, and persist state between sessions.
-import { createHashHistory } from 'history'
-import { routerMiddleware } from 'connected-react-router'
-import createRootReducer from '../reducers'
+import { createHashHistory } from 'history';
+import { routerMiddleware } from 'connected-react-router';
+import createRootReducer from '../reducers';
+import rootSaga from '../sagas';
 
 // https://www.npmjs.com/package/history
-const history = createHashHistory()
-
-const rootReducer = createRootReducer(history)
-const router = routerMiddleware(history)
-// Create the Saga Middleware
-const sagaMiddleware = createSagaMiddleware()
-// use case for middleware is to support asynchronous actions
-// https://redux.js.org/api/applymiddleware
-const enhancer = applyMiddleware(sagaMiddleware, router)
+const history = createHashHistory();
 
 function configureStore(initialState?) {
-  return createStore(rootReducer, initialState, enhancer)
+  const rootReducer = createRootReducer(history);
+  const router = routerMiddleware(history);
+  // Create the Saga Middleware
+  const sagaMiddleware = createSagaMiddleware();
+  // use case for middleware is to support asynchronous actions
+  // https://redux.js.org/api/applymiddleware
+  const enhancer = applyMiddleware(sagaMiddleware, router);
+
+  const store = createStore(rootReducer, initialState, enhancer);
+  sagaMiddleware.run(rootSaga);
+  return store;
 }
 
-//sagaMiddleware.run(mySaga)
-
-export default { configureStore, history }
+export default { configureStore, history };
